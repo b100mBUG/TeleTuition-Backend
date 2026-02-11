b8a286a7da194bc13cc59a8b26a2e0310c6ab34d
b100mBUG <werecastro2006@outlook.com>
2026-02-11 16:55:59 +0300

    I'm the best

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-
-"""DATABASE_URL = (
+DATABASE_URL = (
     "postgresql+asyncpg://neondb_owner:npg_icM2vRHn6qes@"
     "ep-noisy-king-ah8z3emv-pooler.c-3.us-east-1.aws.neon.tech/neondb"
-)"""
+)
 
-DATABASE_URL = "sqlite+aiosqlite:///database.db"
+#DATABASE_URL = "sqlite+aiosqlite:///database.db"
 
 
 engine = create_async_engine(
